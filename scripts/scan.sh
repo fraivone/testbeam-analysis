dfd664fc2da4a6c2fd109fd3604d27c5b7fe0f7c
Antonello Pellecchia <antonello.pellecchia@cern.ch>
2022-02-21 18:49:30 +0100

    Update HV scan script for new unpacker and track reconstruction code formats

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -3,20 +3,23 @@
 # Run an efficiency HV scan with multiple runs
 # TODO: Support rate scan
 
+RUN_DIR=$1 # $EOS_TESTBEAM
+STANDALONE_DIR=$2 # $EOS_TESTBEAM/standalone
+
 #runs=(run_20211102_1056 run_20211102_1210 run_20211102_1349 run_20211102_1609) # tracker hv scan
-#runs=(run_20211030_2213 run_20211030_2317 run_20211031_0016 run_20211031_0213 run_20211031_0222 run_20211031_0321 run_20211031_0503 run_20211031_0606 run_20211031_0707) # ge21 hv scan
+runs=(run_20211030_2213 run_20211030_2317 run_20211031_0016 run_20211031_0213 run_20211031_0222 run_20211031_0321 run_20211031_0503 run_20211031_0606 run_20211031_0707) # ge21 hv scan
 #runs=(run_20211102_1056 run_20211030_2213) # missing runs
-runs=(run_20211030_2054) # runs to be repeated
+#runs=(run_20211030_2054) # runs to be repeated
 
 for run in ${runs[@]}; do
     echo "####################"
     echo "## Analyzing run $run"
     echo "# Unpacker"
-    ./RawToDigi $EOS_TESTBEAM/raw/$run-0-0.raw ferol $EOS_TESTBEAM/standalone/digi/$run.root
+    ./RawToDigi $RUN_DIR/raw/$run-0-0.raw $STANDALONE_DIR/digi/$run.root
     echo "# Local reconstruction"
-    ./DigiToRechits $EOS_TESTBEAM/standalone/digi/$run.root $EOS_TESTBEAM/standalone/rechits/$run.root
+    ./DigiToRechits $STANDALONE_DIR/digi/$run.root $STANDALONE_DIR/rechits/$run.root
     echo "# Tracking"
-    ./Tracking $EOS_TESTBEAM/standalone/rechits/$run.root $EOS_TESTBEAM/standalone/tracks/$run/
+    ./Tracking $STANDALONE_DIR/rechits/$run.root $STANDALONE_DIR/tracks/$run.root
     echo "####################"
     echo ""
     echo ""
