1e3bad6df1d8d8188990475c9b9732dd38a9a86c
Antonello Pellecchia <antonello.pellecchia@cern.ch>
2022-02-22 17:46:49 +0100

    Add efficiency map in scan sh script

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -5,6 +5,7 @@
 
 RUN_DIR=$1 # $EOS_TESTBEAM
 STANDALONE_DIR=$2 # $EOS_TESTBEAM/standalone
+ANALYSIS_DIR=$3 # $EOS_WWW/gem/testbeam/analysis
 
 #runs=(run_20211102_1056 run_20211102_1210 run_20211102_1349 run_20211102_1609) # tracker hv scan
 runs=(run_20211030_2213 run_20211030_2317 run_20211031_0016 run_20211031_0213 run_20211031_0222 run_20211031_0321 run_20211031_0503 run_20211031_0606 run_20211031_0707) # ge21 hv scan
@@ -20,6 +21,8 @@ for run in ${runs[@]}; do
     ./DigiToRechits $STANDALONE_DIR/digi/$run.root $STANDALONE_DIR/rechits/$run.root
     echo "# Tracking"
     ./Tracking $STANDALONE_DIR/rechits/$run.root $STANDALONE_DIR/tracks/$run.root
+    echo "# Analysis"
+    python3 analysis/efficiency.py $STANDALONE_DIR/tracks/$run.root $ANALYSIS_DIR/$run/ ge21 100
     echo "####################"
     echo ""
     echo ""
